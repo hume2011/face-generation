8079de43374ca8112a0ba8c98312023edd7f31cf
huyu <31425652+hume2011@users.noreply.github.com>
2018-04-11 17:37:19 +0800

    Add description

--- dlnd_face_generation.py
+++ dlnd_face_generation.py
@@ -2,15 +2,13 @@
 # coding: utf-8
 
 # # 人脸生成（Face Generation）
-# 在该项目中，你将使用生成式对抗网络（Generative Adversarial Nets）来生成新的人脸图像。
+# 该项目使用生成式对抗网络（Generative Adversarial Nets）来生成新的人脸图像。
 # ### 获取数据
 # 该项目将使用以下数据集：
 # - MNIST
 # - CelebA
 # 
-# 由于 CelebA 数据集比较复杂，而且这是你第一次使用 GANs。我们想让你先在 MNIST 数据集上测试你的 GANs 模型，以让你更快的评估所建立模型的性能。
-# 
-# 如果你在使用 [FloydHub](https://www.floydhub.com/), 请将 `data_dir` 设置为 "/input" 并使用 [FloydHub data ID](http://docs.floydhub.com/home/using_datasets/) "R5KrjnANiKVhLWAkpXhNBe".
+# 第一次使用 GANs,先在 MNIST 数据集上测试你的 GANs 模型，以更快的评估所建立模型的性能。
 
 # In[1]:
 
@@ -32,17 +30,14 @@ helper.download_extract('celeba', data_dir)
 
 # ## 探索数据（Explore the Data）
 # ### MNIST
-# [MNIST](http://yann.lecun.com/exdb/mnist/) 是一个手写数字的图像数据集。你可以更改 `show_n_images` 探索此数据集。
+# [MNIST](http://yann.lecun.com/exdb/mnist/) 是一个手写数字的图像数据集。可以更改 `show_n_images` 探索此数据集。
 
 # In[2]:
 
 
 show_n_images = 36
 
-"""
-DON'T MODIFY ANYTHING IN THIS CELL
-"""
-get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().magic('matplotlib inline')
 import os
 from glob import glob
 from matplotlib import pyplot
@@ -52,29 +47,26 @@ pyplot.imshow(helper.images_square_grid(mnist_images, 'L'), cmap='gray')
 
 
 # ### CelebA
-# [CelebFaces Attributes Dataset (CelebA)](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html) 是一个包含 20 多万张名人图片及相关图片说明的数据集。你将用此数据集生成人脸，不会用不到相关说明。你可以更改 `show_n_images` 探索此数据集。
+# [CelebFaces Attributes Dataset (CelebA)](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html) 是一个包含 20 多万张名人图片及相关图片说明的数据集,用此数据集生成人脸.。
 
 # In[3]:
 
 
 show_n_images = 16
 
-"""
-DON'T MODIFY ANYTHING IN THIS CELL
-"""
+
 mnist_images = helper.get_batch(glob(os.path.join(data_dir, 'img_align_celeba/*.jpg'))[:show_n_images], 28, 28, 'RGB')
 pyplot.imshow(helper.images_square_grid(mnist_images, 'RGB'))
 
 
 # ## 预处理数据（Preprocess the Data）
-# 由于该项目的重点是建立 GANs 模型，我们将为你预处理数据。
 # 
 # 经过数据预处理，MNIST 和 CelebA 数据集的值在 28×28 维度图像的 [-0.5, 0.5] 范围内。CelebA 数据集中的图像裁剪了非脸部的图像部分，然后调整到 28x28 维度。
 # 
 # MNIST 数据集中的图像是单[通道](https://en.wikipedia.org/wiki/Channel_(digital_image%29)的黑白图像，CelebA 数据集中的图像是 [三通道的 RGB 彩色图像](https://en.wikipedia.org/wiki/Channel_(digital_image%29#RGB_Images)。
 # 
 # ## 建立神经网络（Build the Neural Network）
-# 你将通过部署以下函数来建立 GANs 的主要组成部分:
+# 通过部署以下函数来建立 GANs 的主要组成部分:
 # - `model_inputs`
 # - `discriminator`
 # - `generator`
@@ -83,14 +75,11 @@ pyplot.imshow(helper.images_square_grid(mnist_images, 'RGB'))
 # - `train`
 # 
 # ### 检查 TensorFlow 版本并获取 GPU 型号
-# 检查你是否使用正确的 TensorFlow 版本，并获取 GPU 型号
+# 检查是否使用正确的 TensorFlow 版本，并获取 GPU 型号
 
 # In[4]:
 
 
-"""
-DON'T MODIFY ANYTHING IN THIS CELL
-"""
 from distutils.version import LooseVersion
 import warnings
 import tensorflow as tf
@@ -107,7 +96,7 @@ else:
 
 
 # ### 输入（Input）
-# 部署 `model_inputs` 函数以创建用于神经网络的 [占位符 (TF Placeholders)](https://www.tensorflow.org/versions/r0.11/api_docs/python/io_ops/placeholders)。请创建以下占位符：
+# 部署 `model_inputs` 函数以创建用于神经网络的 [占位符 (TF Placeholders)](https://www.tensorflow.org/versions/r0.11/api_docs/python/io_ops/placeholders)：
 # - 输入图像占位符: 使用 `image_width`，`image_height` 和 `image_channels` 设置为 rank 4。
 # - 输入 Z 占位符: 设置为 rank 2，并命名为 `z_dim`。
 # - 学习速率占位符: 设置为 rank 0。
@@ -129,7 +118,7 @@ def model_inputs(image_width, image_height, image_channels, z_dim):
     :param z_dim: The dimension of Z
     :return: Tuple of (tensor of real input images, tensor of z data, learning rate)
     """
-    # TODO: Implement Function
+    
     input_real = tf.placeholder(tf.float32,[None, image_width, image_height,image_channels], name='input_real')
     
     input_z = tf.placeholder(tf.float32, [None,z_dim], name='input_z')
@@ -160,7 +149,7 @@ def discriminator(images, reuse=False, alpha=0.2):
     :param reuse: Boolean if the weights should be reused
     :return: Tuple of (tensor output of the discriminator, tensor logits of the discriminator)
     """
-    # TODO: Implement Function
+    
     with tf.variable_scope('discriminator',reuse=reuse):
         
         x1 = tf.layers.conv2d(images, 64, 5, strides=2, padding='same',
@@ -215,7 +204,7 @@ def generator(z, out_channel_dim, is_train=True, alpha=0.2):
     :param is_train: Boolean if generator is being used for training
     :return: The tensor output of the generator
     """
-    # TODO: Implement Function
+    
     with tf.variable_scope('generator', reuse=not is_train):
         x1 = tf.layers.dense(z, 7*7*512)
         x1 = tf.reshape(x1, (-1, 7, 7, 512))
@@ -259,7 +248,7 @@ tests.test_generator(generator, tf)
 # ### 损失函数（Loss）
 # 部署 `model_loss` 函数训练并计算 GANs 的损失。该函数应返回形如 (discriminator loss, generator loss) 的元组。
 # 
-# 使用你已实现的函数：
+# 使用已实现的函数：
 # - `discriminator(images, reuse=False)`
 # - `generator(z, out_channel_dim, is_train=True)`
 
@@ -274,7 +263,7 @@ def model_loss(input_real, input_z, out_channel_dim, alpha=0.2, smooth=0.1):
     :param out_channel_dim: The number of channels in the output image
     :return: A tuple of (discriminator loss, generator loss)
     """
-    # TODO: Implement Function
+    
     g_model = generator(input_z, out_channel_dim, alpha=alpha)
     d_model_real, d_logits_real = discriminator(input_real, alpha=alpha)
     d_model_fake, d_logits_fake = discriminator(g_model, reuse=True, alpha=alpha)
@@ -312,7 +301,7 @@ def model_opt(d_loss, g_loss, lr, beta1):
     :param beta1: The exponential decay rate for the 1st moment in the optimizer
     :return: A tuple of (discriminator training operation, generator training operation)
     """
-    # TODO: Implement Function
+    
     t_vars = tf.trainable_variables()
     d_vars = [var for var in t_vars if var.name.startswith('discriminator')]
     g_vars = [var for var in t_vars if var.name.startswith('generator')]
@@ -332,14 +321,11 @@ tests.test_model_opt(model_opt, tf)
 
 # ## 训练神经网络（Neural Network Training）
 # ### 输出显示
-# 使用该函数可以显示生成器 (Generator) 在训练过程中的当前输出，这会帮你评估 GANs 模型的训练程度。
+# 使用该函数可以显示生成器 (Generator) 在训练过程中的当前输出，这会帮评估 GANs 模型的训练程度。
 
 # In[10]:
 
 
-"""
-DON'T MODIFY ANYTHING IN THIS CELL
-"""
 import numpy as np
 
 def show_generator_output(sess, n_images, input_z, out_channel_dim, image_mode):
@@ -365,14 +351,12 @@ def show_generator_output(sess, n_images, input_z, out_channel_dim, image_mode):
 
 
 # ### 训练
-# 部署 `train` 函数以建立并训练 GANs 模型。记得使用以下你已完成的函数：
+# 部署 `train` 函数以建立并训练 GANs 模型。使用以下你已完成的函数：
 # - `model_inputs(image_width, image_height, image_channels, z_dim)`
 # - `model_loss(input_real, input_z, out_channel_dim)`
 # - `model_opt(d_loss, g_loss, learning_rate, beta1)`
 # 
 # 使用 `show_generator_output` 函数显示 `generator` 在训练过程中的输出。
-# 
-# **注意**：在每个批次 (batch) 中运行 `show_generator_output` 函数会显著增加训练时间与该 notebook 的体积。推荐每 100 批次输出一次 `generator` 的输出。 
 
 # In[11]:
 
@@ -389,7 +373,7 @@ def train(epoch_count, batch_size, z_dim, learning_rate, beta1, get_batches, dat
     :param data_shape: Shape of the data
     :param data_image_mode: The image mode to use for images ("RGB" or "L")
     """
-    # TODO: Build Model
+    # Build Model
     
     input_real, input_z, lr = model_inputs(data_shape[1], data_shape[2], data_shape[3], z_dim)
     
@@ -406,7 +390,7 @@ def train(epoch_count, batch_size, z_dim, learning_rate, beta1, get_batches, dat
         sess.run(tf.global_variables_initializer())
         for epoch_i in range(epoch_count):
             for batch_images in get_batches(batch_size):
-                # TODO: Train Model
+                # Train Model
                 steps += 1
                 batch_images *= 2
                 # Sample random noise for G
@@ -437,7 +421,7 @@ def train(epoch_count, batch_size, z_dim, learning_rate, beta1, get_batches, dat
 
 
 # ### MNIST
-# 在 MNIST 上测试你的 GANs 模型。经过 2 次迭代，GANs 应该能够生成类似手写数字的图像。确保生成器 (generator) 低于辨别器 (discriminator) 的损失，或接近 0。
+# 在 MNIST 上测试 GANs 模型。经过 2 次迭代，GANs 能够生成类似手写数字的图像。
 
 # In[ ]:
 
@@ -460,7 +444,7 @@ with tf.Graph().as_default():
 
 
 # ### CelebA
-# 在 CelebA 上运行你的 GANs 模型。在一般的GPU上运行每次迭代大约需要 20 分钟。你可以运行整个迭代，或者当 GANs 开始产生真实人脸图像时停止它。
+# 在 CelebA 上运行 GANs 模型。
 
 # In[ ]:
 
@@ -470,10 +454,6 @@ z_dim = 100
 learning_rate = 0.0002
 beta1 = 0.5
 
-
-"""
-DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
-"""
 epochs = 5
 
 celeba_dataset = helper.Dataset('celeba', glob(os.path.join(data_dir, 'img_align_celeba/*.jpg')))
@@ -482,7 +462,4 @@ with tf.Graph().as_default():
           celeba_dataset.shape, celeba_dataset.image_mode)
 
 
-# ### 提交项目
-# 提交本项目前，确保运行所有 cells 后保存该文件。
 # 
-# 保存该文件为 "dlnd_face_generation.ipynb"， 并另存为 HTML 格式 "File" -> "Download as"。提交项目时请附带 "helper.py" 和 "problem_unittests.py" 文件。
